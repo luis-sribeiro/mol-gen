008ff405db365a8d3d2ba211656dfa8aa53b0957
luis-sribeiro <72897006+luis-sribeiro@users.noreply.github.com>
2021-09-06 23:31:08 -0300

    Create pre-training and optim steps for gcpn

--- models/optim_gcpn.py
+++ models/optim_gcpn.py
@@ -3,6 +3,8 @@ from torch import nn, optim
 import torch
 import pickle
 
+
+
 download = True
 
 dataset = None
@@ -24,7 +26,7 @@ if str(device) == 'cuda':
   print('Available GPU: ', torch.cuda.get_device_name(device))
   gpus = (0,)
 else:
-  print('There are no GPUs available')
+  print('There are not GPUs available')
   gpus = None
 
 model = models.RGCN(input_dim=dataset.node_feature_dim,
@@ -33,18 +35,24 @@ model = models.RGCN(input_dim=dataset.node_feature_dim,
 
 model = model.to(device)
 
-task = tasks.GCPNGeneration(model, dataset.atom_types, max_edge_unroll=12,
-                            max_node=38, criterion="nll")
+task = tasks.GCPNGeneration(model, dataset.atom_types,
+                            max_edge_unroll=12, max_node=38,
+                            task="qed", criterion=("ppo", "nll"),
+                            reward_temperature=1,
+                            agent_update_interval=3, gamma=0.9)
 
-optimizer = optim.Adam(task.parameters(), lr = 1e-3)
-
+optimizer = optim.Adam(task.parameters(), lr=1e-5)
 
 solver = core.Engine(task, dataset, None, None, optimizer,
-                     gpus=gpus, batch_size=128, log_interval=10)
+                     gpus=gpus, batch_size=16, log_interval=10)
 
+
+solver.load("gcpn_zinc250k_8.pkl", load_optimizer=False)
 epochs = 10
-print('Pre-training the gcpn model for {} epochs'.format(epochs))
 for epoch in range(epochs):
   solver.train(num_epoch=1)
-  solver.save("gcpn_zinc250k_" + str(epoch) + ".pkl")  
-  print('Model saved!')    
+  solver.save("gcpn_zinc250k_finetune_" + str(epoch) + ".pkl")  
+  print('Model saved!')                 
+
+results = task.generate(num_sample=32, max_resample=5)
+print(results.to_smiles())
